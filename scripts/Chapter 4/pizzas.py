8c01963c5f8ef2ca7f40575ca59f741e49df512c
Judy Chua Co <jtchuaco@gmail.com>
2020-10-23 15:20:07 +0800

    Exercises on creating a for loop

---
+++ scripts/Chapter 4/pizzas.py
@@ -0,0 +1,39 @@
+# exercise 4-1. Pizzas
+# store at least three kinds of pizza names in a list
+# use a for loop to print the name of each pizza
+pizzas = ['pepperoni', 'all meat', 'four cheese']
+for pizza in pizzas:
+    print(pizza)
+
+# modify the for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza
+# example: I like pepperoni pizza.
+pizzas = ['pepperoni', 'all meat', 'four cheese']
+for pizza in pizzas:
+    print("I like " + pizza + " pizza.")
+
+# add another line outside the for loop that states how much you like pizza
+pizzas = ['pepperoni', 'all meat', 'four cheese']
+for pizza in pizzas:
+    print("I like " + pizza + " pizza.")
+
+print("I really love pizza.")
+
+# exercise 4-2. animals
+# make a list of animals that have a common characteristic
+# use a for loop to print out the name of each animal
+animals = ['dog', 'cat', 'fish']
+for animal in animals:
+    print(animal)
+
+# modify your program to print a statement about each animal
+animals = ['dog', 'cat', 'fish']
+for animal in animals:
+    print("A " + animal + " would make a great pet.")
+
+# add a line at the end of your program stating what these animals have in common
+animals = ['dog', 'cat', 'fish']
+for animal in animals:
+    print("A " + animal + " would make a great pet.")
+
+print("Any of these animals would make a great pet.")
+
