1286dccba34d0c303cf340c58f9988aed29aba70
Judy Chua Co <jtchuaco@gmail.com>
2020-10-23 16:14:21 +0800

    How to use a variable and how to correct a variable error

---
+++ scripts/Chapter 2/hello_world.py
@@ -0,0 +1,14 @@
+# using a variable
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+# name error
+message = "Hello Python Crash Course reader!"
+print(mesage)
+
+# correcting the name error
+mesage = "Hello Python Crash Course reader!"
+print(mesage)
