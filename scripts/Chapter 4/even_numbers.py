7a2346b81e44ec1b965609d9339535f968daddc2
Judy Chua Co <jtchuaco@gmail.com>
2020-10-23 16:56:03 +0800

    Using range() function to skip numbers

---
+++ scripts/Chapter 4/even_numbers.py
@@ -0,0 +1,11 @@
+# use range() function to skip numbers
+# list the even numbers between 1 and 10
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+
+# the range() function starts with the value 2 and then adds 2 to that value till it reaches 11
+
+# test: list the odd numbers between 1 and 10
+odd_numbers = list(range(1,11,2))
+print(odd_numbers)
+
