6f124c89d63d3e96b115688827b485326bbdebd4
Judy Chua Co <jtchuaco@gmail.com>
2020-10-30 18:21:26 +0800

    conditional test: checking for inequality

---
+++ scripts/Chapter 5/toppings.py
@@ -0,0 +1,9 @@
+# checking for inequality (!=)
+# ! represents not
+
+requested_topping = 'mushrooms'
+# compares the value of requested_topping to the value of 'anchovies'
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+# because the value of requested_topping is not 'anchovies',
+# the print statement is executed
