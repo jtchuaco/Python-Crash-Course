3d43239c3418049f656d16f562b8c2e25fcfb1c5
Judy Chua Co <jtchuaco@gmail.com>
2020-11-13 16:18:25 +0800

    A dictionary of similar objects: storing the results of a simple poll like favorite programming language

---
+++ scripts/Chapter 6/favorite_languages.py
@@ -0,0 +1,13 @@
+# dictionary of similar objects
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+# you can break up a long print statement
+# choose an appropriate point at which to break what's being printed
+print("Sarah's favorite language is " +
+      favorite_languages['sarah'].title() +
+      ".")
+
