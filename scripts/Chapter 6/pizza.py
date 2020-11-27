ec599ffae1e4872a32fd9f2892a1a97cd686b532
Judy Chua Co <jtchuaco@gmail.com>
2020-11-27 12:11:36 +0800

    A list in a dictionary

---
+++ scripts/Chapter 6/pizza.py
@@ -0,0 +1,30 @@
+# a list in a dictionary
+# store information about a pizza being ordered
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+
+# summarize the order
+print("You ordered a " + pizza['crust'] + "-crust pizza " +
+      "with the following toppings:")
+
+for topping in pizza['toppings']:
+    print("\t" + topping)
+
+# you can nest a list inside a dictionary
+# you can put more than one value to be associated with a single key
+
+# the value associated with each name is now a list
+favorite_languages = {
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
+}
+
+for name, languages in favorite_languages.items():
+    print("\n" +  name.title() + "'s favorite languages are:")
+    for language in languages:
+        print("\t" + language.title())
+
